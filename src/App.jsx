f50159c0deaae9b6f65e88581ddc6100f7c79e92
Zain <thejzainkp@gmail.com>
2023-04-30 03:16:23 +0530

    api connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
-import './App.css'
-import 'primeicons/primeicons.css'
+import "./App.css";
+import "primeicons/primeicons.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css"; 
-import Graph from './components/Graph';
+import "primereact/resources/primereact.min.css";
+import Graph from "./components/Graph";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [achivement, setAchivement] = useState(11);
+  const [postData, setPost] = useState({
+    stock: "msf",
+    achivement: achivement,
+  });
+
+  async function buy() {
+    await axios({
+      method: "post",
+      url: "http://localhost:3000/buy",
+      data: postData,
+    }).then(function (res) {
+      console.log(res);
+    });
+  }
+
+  const lastPrice= (e) => {
+    let newpost = { ...postData, lastprice: e };
+    setPost(newpost);
+  }
 
   return (
-    <div className='p-5'>
-      <Graph />
-      <div className='my-10 flex justify-center gap-5'>
-        <button className='text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>
-          <span className='pi pi-cart-plus'></span> Buy
+    <div className="p-5 ">
+      <div className="h-13 bg-slate-300 flex flex-row-reverse w-[95vw]">
+        <div className="text-lg">{achivement}%</div>
+      </div>
+      <Graph func={lastPrice} />
+      <div className="my-10 flex justify-center gap-5">
+        <button
+          onClick={buy}
+          className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          <span className="pi pi-cart-plus"></span> Buy
         </button>
-        <button className='text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>
-          <span className='pi pi-dollar'></span> Sell
+        <button className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+          <span className="pi pi-dollar"></span> Sell
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,67 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'primereact/chart'
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
+import axios from "axios";
 
-function Graph() {
+function Graph({ func }) {
+  const [time, setTime] = useState([]);
+  const [price, setPrice] = useState([]);
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
 
-    const [chartData, setChartData] = useState({});
-    const [chartOptions, setChartOptions] = useState({});
-
-    useEffect(() => {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('--text-color');
-        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-        const data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
-            datasets: [
-                {
-                    label: 'Price',
-                    data: [65, 59, 80, 81, 56, 55, 40,70,65,44,81,60],
-                    fill: false,
-                    borderColor: documentStyle.getPropertyValue('--blue-500'),
-                    tension: 0.4
-                }
-            ]
-        };
-        const options = {
-            maintainAspectRatio: false,
-            aspectRatio: 0.6,
-            plugins: {
-                legend: {
-                    labels: {
-                        color: textColor
-                    }
-                }
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder
-                    }
-                }
-            }
-        };
-
-        setChartData(data);
-        setChartOptions(options);
-    }, []);
-
-    return (
-      <>
-        <Chart type='line' data={chartData} options={chartOptions}/>
-      </>
-    )
+  function test() {
+    func(price[price.length - 1]);
   }
-  
-  export default Graph
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:3000/getgraph",
+    }).then(function (res) {
+      //   console.log(res.data);
+      setPrice(res.data.arr2);
+      setTime(res.data.arr1);
+      //   console.log(time[time.length-1])
+      test();
+    });
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue("--text-color");
+    const textColorSecondary = documentStyle.getPropertyValue(
+      "--text-color-secondary"
+    );
+    const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
+    const data = {
+      labels: time,
+      datasets: [
+        {
+          label: "Price",
+          data: price,
+          fill: false,
+          borderColor: documentStyle.getPropertyValue("--blue-500"),
+          tension: 0.4,
+        },
+      ],
+    };
+    const options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.6,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+          },
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+          },
+        },
+      },
+    };
+
+    setChartData(data);
+    setChartOptions(options);
+  }, []);
+
+  return (
+    <>
+      <Chart type="line" data={chartData} options={chartOptions} />
+    </>
+  );
+}
+
+export default Graph;
